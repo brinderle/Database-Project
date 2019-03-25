c93c31f4e8b24638a306cf8fe046d8210ce0dbe6
Kemper318 <alk9zt@virginia.edu>
2019-03-25 14:57:10 -0400

    Update User Actions, redirect to action pages

--- queryspecifics.php
+++ queryspecifics.php
@@ -1,10 +1,33 @@
 <html>
     <body>
         <?php
-        if(isset($_POST['submit'])){
-        $selected_val = $_POST['QueryTopic'];  
-        echo "You have selected :" .$selected_val;  
-        }
+
+        echo "You have reached the queryspecifics.php\n";
+
+        $selected_topic = $_POST['Topic'];  
+        $selected_action = $_POST['QueryTopic'];  
+
+        echo "You have selected the action:" .$selected_action ; 
+        echo "You have selected the Topic:" .$selected_topic; 
+
+         if(isset($_POST['QueryTopic']) == "Insert")
+         {
+            header('Location: Insert.html');
+         }
+         if(isset($_POST['QueryTopic']) == "Select")
+         {
+            header('Location: Select.html');
+         }
+         if(isset($_POST['QueryTopic']) == "Update")
+         {
+            header('Location: Update.html');
+         }
+         if(isset($_POST['QueryTopic']) == "Delete")
+         {
+            header('Location: Delete.html');
+         }
+
+        exit;
         ?>
     </body>
 </html>
