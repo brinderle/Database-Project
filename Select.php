e70e5d61e45b532d85751e3b4f7fc02608b540e2
Brandon Rinderle <brrinderle@gmail.com>
2019-03-27 15:00:18 -0400

    trying to pass parameters

--- Select.php
+++ Select.php
@@ -8,8 +8,8 @@
   <body>
     <?php
     session_start();
-    echo 'hello';
-    echo $_SESSION["Topic"];
+    echo 'Your topic is ' $_SESSION["Topic"];
+    echo <br>;
     ?>
     <p>This form links to GetColumns.php where it gets the columns based on the topic you selected on the previous page.  This information should end up being used for the column names that you see underneath this.</p>
     <form action="GetColumns.php" method="post">
