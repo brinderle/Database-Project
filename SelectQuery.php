e54f2c1b20d745b2c6696528a43fbb3fc94e3e74
Brandon Rinderle <brrinderle@gmail.com>
2019-03-28 00:23:52 -0400

    working on dynamic query

--- SelectQuery.php
+++ SelectQuery.php
@@ -20,7 +20,7 @@
     for ($i=0;$i<sizeof($_SESSION['columns']);$i++)
     {
         if ($_SESSION['parameters'][$i] != '') {
-            $sql .= " AND $_SESSION['columns'][$i]";
+            $sql .= " AND " . $_SESSION['columns'][$i] "=" . $_SESSION['parameters'][$i];
         }
     }
     echo $sql;
