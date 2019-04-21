ba9bc179c5b71f3c5a617ffcacca93a722255df7
Brandon Rinderle <brrinderle@gmail.com>
2019-04-21 16:52:59 -0400

    trying prepared statements

--- SelectQuery.php
+++ SelectQuery.php
@@ -59,7 +59,7 @@
                 // assume int
                 $type_string .= "i";
             }
-            array_push($parameters, $_SESSION['parameters'][$i]);
+            array_push($parameters, &$_SESSION['parameters'][$i]);
             // $parameters[] = &$_SESSION['parameters'][$i];
         }
     }
