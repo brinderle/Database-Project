c5c53f2e840acfff64fab6af8d5168d3086a9dcf
Brandon Rinderle <brrinderle@gmail.com>
2019-04-21 14:20:32 -0400

    trying prepared statements

--- SelectQuery.php
+++ SelectQuery.php
@@ -23,17 +23,47 @@
         array_push($_SESSION['operators'], $_POST[$value . 'ID']);
     }
     // append the conditions to the where clause if there was a value entered for that field
+    // for ($i=0;$i<sizeof($_SESSION['columns']);$i++)
+    // {
+    //     if ($_SESSION['parameters'][$i] != '') {
+    //         $sql .= " AND " . $_SESSION['columns'][$i] . $_SESSION['operators'][$i];
+    //         if ($_SESSION['column_data_types'][$i] == "varchar" or $_SESSION['column_data_types'][$i] == "datetime") {
+    //             $sql .= '"' . $_SESSION['parameters'][$i] . '"';
+    //         } else {
+    //             $sql .= $_SESSION['parameters'][$i];
+    //         }
+    //     }
+    // }
     for ($i=0;$i<sizeof($_SESSION['columns']);$i++)
     {
         if ($_SESSION['parameters'][$i] != '') {
-            $sql .= " AND " . $_SESSION['columns'][$i] . $_SESSION['operators'][$i];
-            if ($_SESSION['column_data_types'][$i] == "varchar" or $_SESSION['column_data_types'][$i] == "datetime") {
-                $sql .= '"' . $_SESSION['parameters'][$i] . '"';
-            } else {
-                $sql .= $_SESSION['parameters'][$i];
-            }
+            $sql .= " AND " . $_SESSION['columns'][$i] . $_SESSION['operators'][$i] . ' ?';
+            // if ($_SESSION['column_data_types'][$i] == "varchar" or $_SESSION['column_data_types'][$i] == "datetime") {
+            //     $sql .= '"' . $_SESSION['parameters'][$i] . '"';
+            // } else {
+            //     $sql .= $_SESSION['parameters'][$i];
+            // }
         }
     }
+    // get the datatypes and bind parameters to the query
+    $type_string = "";
+    $parameters = array();
+    for ($i=0;$i<sizeof($_SESSION['columns']);$i++) {
+        if ($_SESSION['parameters'][$i] != '') {
+            if ($_SESSION['column_data_types'][$i] == "varchar" or $_SESSION['column_data_types'][$i] == "datetime") {
+                $type_string .= "s";
+            } else if ($_SESSION['column_data_types'][$i] == "double" or $_SESSION['column_data_types'][$i] == "float") {
+                $type_string .= "d";
+            } else {
+                // assume int
+                $type_string .= "i";
+            }
+            array_push($parameters, $_SESSION['parameters'][$i]);
+        }
+    }
+
+    $sql->bind_param( $type_string, $parameters );
+
     echo $sql . "<br>";
     echo "The results from the query are shown below.  Click the Export Data button if you would like to export this data to a csv file.";
     echo "<br>";
@@ -42,7 +72,8 @@
     // get result and format it as a table
     echo "<table>";
     echo "<tr>";
-    $result = mysqli_query($con,$sql);
+    // $result = mysqli_query($con,$sql);
+    $result = $sql->execute();
     $_SESSION['result'] = $result;
     $_SESSION['query'] = $sql;
     // Print the data from the table row by row
