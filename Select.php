a07c7a4a0fdc7592a124b9d2855afe6ea1c22d7b
Kemper318 <alk9zt@virginia.edu>
2019-04-24 11:47:55 -0400

    Add background image, fix text

--- Select.php
+++ Select.php
@@ -6,7 +6,7 @@
     <meta charset="UTF-8">
     <title>Query</title>
   </head>
-  <body style="text-align: center">
+  <body style="background-image:url('carousel.jpg');  background-size: 100%; text-align: center">
     <nav class="navbar navbar-inverse">
   <div class="container-fluid">
     <!-- Brand and toggle get grouped for better mobile display -->
@@ -35,6 +35,11 @@
     </div><!-- /.navbar-collapse -->
   </div><!-- /.container-fluid -->
 </nav>
+<div id="content-container" class="container-fluid" style="align-content: center">
+    <div class="row">
+        <div class="col-sm-12 jumbotron"
+             style=" background-color: white; max-width: fit-content; float: none; margin: 0 auto;">
+
     <!-- this is the php section to get the column names for the topic and put them in the session array -->
     <?php
     require_once('./library.php');
@@ -73,7 +78,8 @@
     ?>
     <br>
     <br>
-    <p>Fill out this form with the values you would like to query on for each field.  If you do not input any values into the fields, submitting will return all of the data for this topic.</p>
+    <p>Fill out this form with the values you would like to query on for each field.  </p>
+    <p>If you do not input any values into the fields, submitting will return all of the data for this topic.</p>
     <!-- this section dynamically titles the form -->
     <?php
     echo "<form action='SelectQuery.php' method='post'>";
@@ -94,5 +100,9 @@
     <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-    </body>
+       
+</div>
+    </div>
+</div>
+ </body>
 </html>
